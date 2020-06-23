eecfc4aa835f53d5dfc4acef04f3e0edf5dd8ccf
Ayush garg <67330316+ayushgarg15@users.noreply.github.com>
2020-06-23 22:01:30 +0530

    Add files via upload

---
+++ pattern8.java
@@ -0,0 +1,24 @@
+// pattern 7
+//         *
+//       *
+//     *
+//   *
+// *
+import java.util.*;
+class pattern8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(j==(n-i)+1){
+                    System.out.print("*" + " ");
+                }else{
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
