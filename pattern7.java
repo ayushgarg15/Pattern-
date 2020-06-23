d99b707ba6227655223db992f6aa705e326d0a8c
Ayush garg <67330316+ayushgarg15@users.noreply.github.com>
2020-06-23 22:01:11 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,24 @@
+// pattern 7
+// *
+//   *
+//     *
+//       *
+//         *
+import java.util.*;
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==j){
+                    System.out.print("*" + " ");
+                }else{
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
